e4a6b1927fc4a1c7f616bfa9c29e77365a86d387
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2023-04-24 22:50:03 +0100

    monitor already have executed and executing arrays

--- src/monitor.c
+++ src/monitor.c
@@ -9,25 +9,58 @@
 #include <errno.h>
 
 #define BUFFSIZE 1024
- 
+
 int main(int argc, char **argv) {
 
-    if (mkfifo("../tmp/fifo.txt", 0600) < 0) {
+    if (mkfifo("tmp/fifo", 0600) < 0) {
         write(2, "Error creating fifo.", 21);
         exit(EXIT_FAILURE);
     }
 
-    int fifo = open("../tmp/fifo.txt", O_RDONLY);
+    int fifo = open("tmp/fifo", O_RDONLY);
         if (fifo < 0) {
             write(2, "Error opening fifo.", 20);
             exit(EXIT_FAILURE);
         }
 
+    typedef struct process {
+        int pid;
+        char *prog_name;
+        long int secs;
+        long int milis;
+    } process;
+
+    process executing[100]; int nr_executing = 0;
+    process executed[100]; int nr_executed = 0;
+
     int leitura;
     char buf[BUFFSIZE];
     while(1) {
         while ((leitura = read(fifo, buf, BUFFSIZE)) > 0) {
-            
+            char *status = strtok(buf,";");
+
+            process this_process = {
+                atoi(strtok(NULL, ";")),
+                strtok(NULL, ";"),
+                atol(strtok(NULL, ";")),
+                atol(strtok(NULL, ";"))
+            };
+
+            if (strcmp(status,"executing") == 0) executing[nr_executing++] = this_process;
+            if (strcmp(status,"executed") == 0) {
+                executed[nr_executed++] = this_process;
+
+                int index = -1;
+                for(int i = 0; i < nr_executing; i++) {
+                    if (executing[i].pid == this_process.pid) index = i;
+                }
+
+                for(int i = index; i < nr_executing; i++) {
+                    executing[i] = executing[i+1];
+                }
+
+                nr_executing--;
+            }
         }
     }
 }
--- src/tracer.c
+++ src/tracer.c
@@ -9,18 +9,23 @@
 #include <errno.h>
 #include <ctype.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 
 #define BUFSIZE 1024
 
-void execute_u(char *args) {
+void execute_u(char *args, int fifo) {
     pid_t pid = getpid();
     char pid_write[20]; sprintf(pid_write, "PROCESS ID: %d\n", pid);
     write(1, pid_write, strlen(pid_write));
 
+    char *prog_name = strtok(args, " ");
+    struct timeval tv; gettimeofday(&tv, NULL);
+    char send_fifo[BUFSIZ]; sprintf(send_fifo, "executing;%d;%s;%ld;%ld", pid, prog_name, tv.tv_sec, tv.tv_usec);
+    write(fifo, send_fifo, BUFSIZE);
+
     char executing[BUFSIZE]; sprintf(executing, "EXECUTING \"%s\"\n", args);
     write(1, executing, strlen(executing));
 
-    char *prog_name = strtok(args, " ");
     char *arg_str = strtok(NULL, "");
     char **args_list = NULL;
     int num_args = 0;
@@ -32,15 +37,23 @@ void execute_u(char *args) {
         arg = strtok(NULL, " ");
     }
 
-    execvp(prog_name, args_list);
+    int fork_child = fork();
+    if (fork_child == -1) write(STDERR_FILENO, "Error creating child process.", 30);
+    else if (fork_child == 0) execvp(prog_name, args_list);
+    else {
+        wait(NULL);
 
-    for (int i = 0; i < num_args; i++) {
-        free(args_list[i]);
+        gettimeofday(&tv, NULL);
+        char send_fifo[BUFSIZ]; sprintf(send_fifo, "executed;%d;%s;%ld;%ld", pid, prog_name, tv.tv_sec, tv.tv_usec);
+        write(fifo, send_fifo, BUFSIZE);
+
+        for (int i = 0; i < num_args; i++) {
+            free(args_list[i]);
+        }
+        free(args_list);
+        free(arg_str);
+        free(arg);
     }
-    free(args_list);
-    free(prog_name);
-    free(arg_str);
-    free(arg);
 }
 
 char *strtrim(char *str) {
@@ -59,7 +72,7 @@ char *strtrim(char *str) {
     return str;
 }
 
-void execute_p(char *args) {
+void execute_p(char *args, int fifo) {
     char *token = strtok(args, "|");
     int count = 0;
     char *trimmed;
@@ -76,7 +89,7 @@ void execute_p(char *args) {
         if (pid_child == 0) {
             char print[50]; sprintf(print, "----------------------------\nINPUT Nº %d\n", count);
             write(1, print, strlen(print));
-            execute_u(trimmed);
+            execute_u(trimmed, fifo);
         }
         else wait(NULL);
     }
@@ -101,15 +114,15 @@ int main(int argc, char **argv) {
     }
 
     else if (argc == 4) {
-        int fifo = open("../tmp/fifo", O_WRONLY);
+        int fifo = open("tmp/fifo", O_WRONLY);
             if (fifo < 0) {
                 write(2, "Error opening fifo.", 20);
                 exit(EXIT_FAILURE);
             }
 
         if (strcmp(argv[1], "execute") == 0) {
-            if (strcmp(argv[2], "-u") == 0) execute_u(argv[3]);
-            else if (strcmp(argv[2], "-p") == 0) execute_p(argv[3]);
+            if (strcmp(argv[2], "-u") == 0) execute_u(argv[3], fifo);
+            else if (strcmp(argv[2], "-p") == 0) execute_p(argv[3], fifo);
             else {
                 write(STDERR_FILENO, "Invalid execute argument.\n", 27);
                 exit(EXIT_FAILURE);
