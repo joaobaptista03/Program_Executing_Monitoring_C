6b6c30328826e1953e4462d1fd0934277e584e4d
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2023-04-25 02:25:06 +0100

    theres an error in exec time (main or monitor)

--- src/monitor.c
+++ src/monitor.c
@@ -23,21 +23,27 @@ int main(int argc, char **argv) {
             exit(EXIT_FAILURE);
         }
 
-    typedef struct process {
+    typedef struct executing_process {
         int pid;
         char *prog_name;
         long int secs;
         long int milis;
-    } process;
+    } executing_process;
 
-    process *executing = malloc(sizeof(process) * 10); 
+    typedef struct executed_process {
+        int pid;
+        char *prog_name;
+        float exec_time;
+    } executed_process;
+
+    executing_process *executing = malloc(sizeof(executing_process) * 10); 
     if (executing == NULL) {
         printf("Failed to allocate memory for executing array.");
         exit(EXIT_FAILURE);
     }
     int nr_executing = 0, capacity_executing = 10;
 
-    process *executed = malloc(sizeof(process) * 10); 
+    executed_process *executed = malloc(sizeof(executed_process) * 10); 
     if (executed == NULL) {
         printf("Failed to allocate memory for executed array.");
         exit(EXIT_FAILURE);
@@ -51,7 +57,7 @@ int main(int argc, char **argv) {
         while ((leitura = read(fifo, buf, BUFFSIZE)) > 0) {
             char *status = strtok(buf,";");
 
-            process this_process = {
+            executing_process this_process = {
                 atoi(strtok(NULL, ";")),
                 strtok(NULL, ";"),
                 atol(strtok(NULL, ";")),
@@ -62,7 +68,7 @@ int main(int argc, char **argv) {
                 if (nr_executing < capacity_executing) executing[nr_executing++] = this_process;
                 else {
                     capacity_executing *= 2;
-                    executing = realloc(executing, capacity_executing * sizeof(process));
+                    executing = realloc(executing, capacity_executing * sizeof(executing_process));
                     if (executing == NULL) {
                         printf("Failed to allocate memory for executing array.");
                         exit(EXIT_FAILURE);
@@ -72,15 +78,35 @@ int main(int argc, char **argv) {
             }
 
             if (strcmp(status,"executed") == 0) {
-                if (nr_executed < capacity_executed) executed[nr_executed++] = this_process;
+                
+                float exec_time = 0;
+                for(int i = 0; i < nr_executing; i++) {
+                    if (executing[i].pid == this_process.pid) {
+                        if (this_process.secs == executing[i].secs) exec_time = (float) (this_process.milis - executing[i].milis) / (float) 1000;
+                        else {
+                            exec_time = ((float) this_process.milis + (float) (1000000 - executing[i].milis)) / (float) 1000;
+                        }
+                    }
+                }
+
+                this_process.secs = 0;
+                this_process.milis = exec_time;
+
+                executed_process new_executed = {
+                    this_process.pid,
+                    this_process.prog_name,
+                    exec_time
+                };
+
+                if (nr_executed < capacity_executed) executed[nr_executed++] = new_executed;
                 else {
                     capacity_executed *= 2;
-                    executed = realloc(executed, capacity_executed * sizeof(process));
+                    executed = realloc(executed, capacity_executed * sizeof(executed_process));
                     if (executed == NULL) {
                         printf("Failed to allocate memory for executed array.");
                         exit(EXIT_FAILURE);
                     }
-                    executed[nr_executed++] = this_process;
+                    executed[nr_executed++] = new_executed;
                 }
 
                 int index = -1;
@@ -94,6 +120,8 @@ int main(int argc, char **argv) {
 
                 nr_executing--;
             }
+
+            for(int i = 0; i < nr_executed; i++) printf("executed in %f milliseconds\n", executed[i].exec_time);
         }
     }
 
--- src/tracer.c
+++ src/tracer.c
@@ -10,21 +10,22 @@
 #include <ctype.h>
 #include <sys/wait.h>
 #include <sys/time.h>
+#include <stdbool.h>
 
 #define BUFSIZE 1024
 
-void execute_u(char *args, int fifo) {
+void execute_u(char *args, int fifo, bool pipeline) {
     pid_t pid = getpid();
     char pid_write[20]; sprintf(pid_write, "PROCESS ID: %d\n", pid);
     write(1, pid_write, strlen(pid_write));
+
+    char executing[BUFSIZE]; sprintf(executing, "EXECUTING \"%s\"\n", args);
+    write(1, executing, strlen(executing));
 
     char *prog_name = strtok(args, " ");
     struct timeval tv; gettimeofday(&tv, NULL);
     char send_fifo[BUFSIZ]; sprintf(send_fifo, "executing;%d;%s;%ld;%ld", pid, prog_name, tv.tv_sec, tv.tv_usec);
     write(fifo, send_fifo, BUFSIZE);
-
-    char executing[BUFSIZE]; sprintf(executing, "EXECUTING \"%s\"\n", args);
-    write(1, executing, strlen(executing));
 
     char *arg_str = strtok(NULL, "");
     char **args_list = NULL;
@@ -37,38 +38,43 @@ void execute_u(char *args, int fifo) {
         arg = strtok(NULL, " ");
     }
 
-    int fork_child = fork();
-    if (fork_child == -1) write(STDERR_FILENO, "Error creating child process.", 30);
-    else if (fork_child == 0) execvp(prog_name, args_list);
+    if (pipeline) execvp(prog_name, args_list);
     else {
-        wait(NULL);
-
-        gettimeofday(&tv, NULL);
-        char send_fifo[BUFSIZ]; sprintf(send_fifo, "executed;%d;%s;%ld;%ld", pid, prog_name, tv.tv_sec, tv.tv_usec);
-        write(fifo, send_fifo, BUFSIZE);
-
-        for (int i = 0; i < num_args; i++) {
-            free(args_list[i]);
+        int fork_child = fork();
+        if (fork_child == -1) {
+            write(2, "Error creating child process.", 30);
+            exit(EXIT_FAILURE);
         }
-        free(args_list);
-        free(arg_str);
-        free(arg);
+        else if (fork_child == 0) execvp(prog_name, args_list);
+        else wait(NULL);
     }
+
+    gettimeofday(&tv, NULL);
+    char send_fifo2[BUFSIZ]; sprintf(send_fifo2, "executed;%d;%s;%ld;%ld", pid, prog_name, tv.tv_sec, tv.tv_usec);
+    write(fifo, send_fifo2, BUFSIZE);
+
+    for (int i = 0; i < num_args; i++) {
+        free(args_list[i]);
+    }
+    free(args_list);
+    free(arg_str);
+    free(arg);
+
 }
 
 char *strtrim(char *str) {
     char *end;
-    while (isspace(*str)) { // skip leading whitespace
+    while (isspace(*str)) {
         str++;
     }
-    if (*str == '\0') { // empty string
+    if (*str == '\0') {
         return str;
     }
     end = str + strlen(str) - 1;
-    while (end > str && isspace(*end)) { // skip trailing whitespace
+    while (end > str && isspace(*end)) {
         end--;
     }
-    *(end + 1) = '\0'; // terminate the trimmed string
+    *(end + 1) = '\0';
     return str;
 }
 
@@ -79,7 +85,6 @@ void execute_p(char *args, int fifo) {
     while (token != NULL) {
         count++;
         trimmed = strtrim(token);
-        token = strtok(NULL, "|");
 
         int pid_child = fork();
         if (pid_child == -1) {
@@ -89,14 +94,13 @@ void execute_p(char *args, int fifo) {
         if (pid_child == 0) {
             char print[50]; sprintf(print, "----------------------------\nINPUT Nº %d\n", count);
             write(1, print, strlen(print));
-            execute_u(trimmed, fifo);
+            execute_u(trimmed, fifo, true);
         }
         else wait(NULL);
+            
+
+        token = strtok(NULL, "|");
     }
-
-    puts("");
-
-    free(token);
 }
 
 void status() {
@@ -121,7 +125,7 @@ int main(int argc, char **argv) {
             }
 
         if (strcmp(argv[1], "execute") == 0) {
-            if (strcmp(argv[2], "-u") == 0) execute_u(argv[3], fifo);
+            if (strcmp(argv[2], "-u") == 0) execute_u(argv[3], fifo, false);
             else if (strcmp(argv[2], "-p") == 0) execute_p(argv[3], fifo);
             else {
                 write(STDERR_FILENO, "Invalid execute argument.\n", 27);
@@ -142,7 +146,7 @@ int main(int argc, char **argv) {
     /* PROGRAM RUN TIME CALCULATION */
     clock_t end = clock();
     double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("PROGRAM FINISHED (%f seconds)\n\n", cpu_time_used );
+    printf("\nPROGRAM FINISHED (%f seconds)\n\n", cpu_time_used );
 
     return 0;
 }
